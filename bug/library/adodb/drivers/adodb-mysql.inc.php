96b89e862d0cd7532c81999e5c39de14521bc657
jimmy <jimmy.dong@gmail.com>
2017-06-10 14:09:41 +0800

    update

--- bug/library/adodb/drivers/adodb-mysql.inc.php
+++ bug/library/adodb/drivers/adodb-mysql.inc.php
@@ -361,7 +361,7 @@ class ADODB_mysql extends ADOConnection {
 		if (!empty($this->port)) $argHostname .= ":".$this->port;
 		
 		if (ADODB_PHPVER >= 0x4300)
-			$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword,
+			@$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword,
 												$this->forceNewConnect,$this->clientFlags);
 		else if (ADODB_PHPVER >= 0x4200)
 			$this->_connectionID = mysql_connect($argHostname,$argUsername,$argPassword,
