ad9b5acb18cfbd3cba750f2aaed4f227f02a5c47
jimmy <jimmy.dong@gmail.com>
2017-09-29 12:27:08 +0800

    debug

--- db_info/trans.php
+++ db_info/trans.php
@@ -5,8 +5,7 @@
  */
 include("db_info.ini.php");
 
-$re = ['success'=>false, 'msg'=>'error'];
-switch($_REQUEST['todo']){
+switch($_REQUEST['do']){
 	case 'toArray': //描述转数组。适用： 0：文本回复 1：图文回复 2：音乐回复 3：视频回复
 		$code = $_REQUEST['code'];
 		$code = str_replace('：',':',$code);
@@ -14,11 +13,16 @@ switch($_REQUEST['todo']){
 		foreach($reg as $v){
 			$re[$v[1]] = $v[2];
 		}
-		$re = ['success'=>true, 'data'=>var_export($re, true)];
+		$re = var_export($re, true);
 		break;
 	default:
-		$re = ['success'=>false, 'msg'=>'错误：没有转换指令'];
+		$re = '错误：没有转换指令';
 		break;
 }
+showhead();
+print <<< end_of_print
+<pre>
+{$re}
+</pre>
 
-echo json_encode($re, JSON_UNESCAPED_UNICODE);
+end_of_print;
