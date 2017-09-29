79e1ecd3ca189b5b3eddb9a882ac3ac1572ef07b
jimmy <jimmy.dong@gmail.com>
2017-09-29 12:37:24 +0800

    debug

--- db_info/trans.php
+++ db_info/trans.php
@@ -9,9 +9,9 @@ switch($_REQUEST['do']){
 	case 'toArray': //描述转数组。适用： 0：文本回复 1：图文回复 2：音乐回复 3：视频回复
 		$code = $_REQUEST['code'];
 		$code = str_replace('：',':',$code);
-		$code = str_replace(' ','',$code);
-		$code = str_replace('　','',$code);
-		preg_match_all('/([0-9]+)(\:|=>)([^\s]+)/', $code, $reg, PREG_SET_ORDER);
+		$code = str_replace('　',' ',$code);
+		$code = str_replace('  ',' ',$code);
+		preg_match_all('/([0-9]+)\s?(\:|=>)\s?([^\s]+)/', $code, $reg, PREG_SET_ORDER);
 		foreach($reg as $v){
 			$re[$v[1]] = $v[3];
 		}
