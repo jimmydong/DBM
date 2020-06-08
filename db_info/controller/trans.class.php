ba2d93da8d4c342017dee4712887bf20a7792e62
jimmy.dong <jimmy.dong@gmail.com>
2020-06-08 12:50:59 +0800

    debug

--- db_info/controller/trans.class.php
+++ db_info/controller/trans.class.php
@@ -158,15 +158,10 @@ class Trans extends Base {
 	 */
 	public function _trans($code){
 		$code = str_replace('：', ':', $code);
-		$code = str_replace(',', ' ', $code);
-		$code = str_replace('，', ' ', $code);
-		$code = str_replace('　', ' ', $code);
-		$code = str_replace('  ', ' ', $code);
-		$code = str_replace("'", '', $code);
-		$code = str_replace('"', '', $code);
+		$code = str_replace([',', '，', '　', '  ', "'", '"'], '', $code);
 		$code = str_replace("（", '(', $code);
 		$code = str_replace('）', ')', $code);
-		preg_match_all('/(-?[0-9]+)\s?(\:|=>)\s?([^\s]+)/', $code, $reg, PREG_SET_ORDER);
+		preg_match_all('/(-?[0-9]+)\s?(\:|=>)\s?([^\s]+[\)]?)/', $code, $reg, PREG_SET_ORDER);
 		foreach($reg as $v){
 			$re[$v[1]] = $v[3];
 		}
